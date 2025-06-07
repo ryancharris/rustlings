e6bc6e0850189d76f80307cf9c93658c7a5fb2a6
Ryan Harris <ryan.harris@dbtlabs.com>
2025-06-07 13:55:00 -0400

    init commit

---
+++ exercises/02_functions/functions3.rs
@@ -0,0 +1,11 @@
+fn call_me(num: u8) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
+
+fn main() {
+    // TODO: Fix the function call.
+    let x: u8 = 5;
+    call_me(x);
+}
---
+++ exercises/03_if/if2.rs
@@ -0,0 +1,47 @@
+fn picky_eater(food: &str) -> &str {
+    if food == "strawberry" {
+        "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
+    } else {
+        let disliked_foods: [&str; 3] = [
+            "broccoli",
+            "gummy bears",
+            "literally anything",
+        ];
+        if disliked_foods.contains(&food) {
+            "No thanks!"
+        } else {
+            "I don't know about that."
+        }
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+// TODO: Read the tests to understand the desired behavior.
+// Make all tests pass without changing them.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn yummy_food() {
+        // This means that calling `picky_eater` with the argument "food" should return "Yummy!".
+        assert_eq!(picky_eater("strawberry"), "Yummy!");
+    }
+
+    #[test]
+    fn neutral_food() {
+        assert_eq!(picky_eater("potato"), "I guess I can eat that.");
+    }
+
+    #[test]
+    fn default_disliked_food() {
+        assert_eq!(picky_eater("broccoli"), "No thanks!");
+        assert_eq!(picky_eater("gummy bears"), "No thanks!");
+        assert_eq!(picky_eater("literally anything"), "No thanks!");
+    }
+}
