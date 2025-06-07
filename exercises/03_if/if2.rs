f782032929abc48d3bc1009283ca4efd5231d352
Ryan Harris <ryan.harris@dbtlabs.com>
2025-06-07 14:02:47 -0400

    finish excercise 3

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -21,15 +21,12 @@ fn main() {
     // You can optionally experiment here.
 }
 
-// TODO: Read the tests to understand the desired behavior.
-// Make all tests pass without changing them.
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn yummy_food() {
-        // This means that calling `picky_eater` with the argument "food" should return "Yummy!".
         assert_eq!(picky_eater("strawberry"), "Yummy!");
     }
 
